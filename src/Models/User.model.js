49443a381040687935b6404369fff3ce5c977a4a
asim093 <asimusman8899@gmail.com>
2024-12-28 01:48:29 -0800

    backend boilerplate

---
+++ src/Models/User.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Userschema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profileImage : {type : String , required : true},
+  token: { type: String, default: "" },
+  otp: {
+    value: { type: String },
+    expireAt: { type: Date },
+    verified: { type: Boolean, default: "false" },
+  },
+});
+
+
+export const Usermodle = mongoose.model("User" , Userschema);
